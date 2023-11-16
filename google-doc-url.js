a2dc47baa62a816dd07d5fd1122f79ddd56b9320
alcaban <143461668+alcaban@users.noreply.github.com>
2023-11-15 21:57:23 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Pq7xlsJjSVCGX65SdGph-lx4gKk4QaAlViQnMVIpz_s/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTn4b64Zaoj3R3fusz2nUeTR-jwMgRfFMxttKn0RhEjPETxiJl_cUaa3QGuY5W-bt7aK5p-FQ7xSYrx/pub?output=csv';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
